a18d72c5adb35c9da1c40c488c184ab7e3072970
Jae Kim <sekero1@daum.net>
2023-04-13 11:26:37 +0900

    Add files via upload

--- mqtt/save_msg.py
+++ mqtt/save_msg.py
@@ -1,3 +1,13 @@
+import sys
+sys.path.append('/home/jae/myprj/mysite')  #  Python search 디렉토리 경로 추가
+
+# To use Django objects in the file which are not created from startapps command, add DJANGO_SETTING_MODULE to the system environemnt 
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
+import django
+django.setup()
+
 from daq.models import Tnh
 
 def save_db(msg):
@@ -6,6 +16,3 @@ def save_db(msg):
     db_tab1.humidity = float(msg.payload)
 
     print(db_tab1.temperature, db_tab1.humidity)
-
-
-
